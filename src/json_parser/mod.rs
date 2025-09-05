25fea6949280995ceea573ea55599e2180ba5ada
Aditya Saxena <adity09@outlook.com>
2025-09-05 17:46:09 +0530

    array/object parsing pending , shallow tested

--- src/json_parser/mod.rs
+++ src/json_parser/mod.rs
@@ -2,17 +2,6 @@ use std::collections::HashMap;
 
 const NOT_VALID_JSON_FORMAT: &str = "The provided data is not in a valid JSON format!";
 
-/*enum ParsePhase {
-    KeyString,
-    ValueString,
-    ValueNumber,
-    ValueBoolean,
-    ValueNull,
-    ValueArray,
-    ValueObject,
-    ObjectSpace,
-}*/
-
 pub struct JsonParser {
     text: String,
     map: HashMap<String, String>,
@@ -61,30 +50,16 @@ impl JsonParser {
         while ind < chars_arr.len() {
             let ch: char = chars_arr[ind];
 
-            if unicode_rem_cnt < 0 || unicode_rem_cnt > 4 {
-                // e
-                println!(
-                    "{}",
-                    "Some error occurred while parsing the JSON data!\n
-                        Possible causes:\n\t- Invalid unicode sequence!"
-                );
-                return Err(NOT_VALID_JSON_FORMAT);
-            }
-
             if unicode_rem_cnt > 0 {
                 parsed_unicode_str.push(ch);
                 unicode_rem_cnt -= 1;
 
-                if unicode_rem_cnt != 0 {
+                if unicode_rem_cnt > 0 {
                     ind += 1;
                     continue;
                 }
 
-                if parsed_unicode_str.len() != 4 {
-                    // e
-                    println!("{}", "Invalid unicode sequence encountered!");
-                    return Err(NOT_VALID_JSON_FORMAT);
-                }
+                let mut is_valid_unicode_seq: bool = true;
 
                 match u32::from_str_radix(&parsed_unicode_str, 16) {
                     Ok(num) => {
@@ -92,28 +67,30 @@ impl JsonParser {
                             *parsed_unicode_str = code.to_string();
                             break;
                         } else {
-                            // e
-                            println!("{}", "Invalid unicode sequence encountered!");
-                            return Err(NOT_VALID_JSON_FORMAT);
+                            is_valid_unicode_seq = false;
                         }
                     }
-                    Err(err) => {
-                        // e
-                        println!("{}: {}", "Invalid unicode sequence encountered!", err);
-                        return Err(NOT_VALID_JSON_FORMAT);
+                    Err(_err) => {
+                        is_valid_unicode_seq = false;
                     }
+                }
+
+                if !is_valid_unicode_seq {
+                    // e
+                    println!("{}", "Invalid unicode sequence encountered!");
+                    return Err(NOT_VALID_JSON_FORMAT);
                 }
             }
 
             let escaped_char = match ch {
-                '"' => '"',
-                '\\' => '\\',
-                '/' => '/',
-                'n' => '\n',
-                'r' => '\r',
-                't' => '\t',
-                'b' => '\x08',
-                'f' => '\x0C',
+                '"' => "\"",
+                '\\' => "\\",
+                '/' => "/",
+                'n' => "\n",
+                'r' => "\r",
+                't' => "\t",
+                'b' => "\x08",
+                'f' => "\x0C",
                 'u' => {
                     unicode_rem_cnt = 4;
                     ind += 1;
@@ -166,55 +143,132 @@ impl JsonParser {
         return Ok(ind);
     }
 
+    fn is_null_or_bool_sequence(chars_arr: &Vec<char>, ind: usize) -> Result<String, &'static str> {
+        if chars_arr[ind] == 'n' {
+            let is_null: bool = (ind + 4) < chars_arr.len()
+                && chars_arr[ind] == 'n'
+                && chars_arr[ind + 1] == 'u'
+                && chars_arr[ind + 2] == 'l'
+                && chars_arr[ind + 3] == 'l';
+
+            if is_null {
+                return Ok(String::from("null"));
+            }
+        } else if chars_arr[ind] == 't' {
+            let is_true: bool = (ind + 4) < chars_arr.len()
+                && chars_arr[ind] == 't'
+                && chars_arr[ind + 1] == 'r'
+                && chars_arr[ind + 2] == 'u'
+                && chars_arr[ind + 3] == 'e';
+
+            if is_true {
+                return Ok(String::from("true"));
+            }
+        } else if chars_arr[ind] == 'f' {
+            let is_false: bool = (ind + 5) < chars_arr.len()
+                && chars_arr[ind] == 'f'
+                && chars_arr[ind + 1] == 'a'
+                && chars_arr[ind + 2] == 'l'
+                && chars_arr[ind + 3] == 's'
+                && chars_arr[ind + 4] == 'e';
+
+            if is_false {
+                return Ok(String::from("false"));
+            }
+        }
+
+        return Err(NOT_VALID_JSON_FORMAT);
+    }
+
+    fn is_num_sequence(chars_arr: &Vec<char>, mut ind: usize) -> Result<String, &'static str> {
+        let mut val_str: String = String::new();
+        let mut have_decimal_point: bool = false;
+        let mut have_exp_char: bool = false;
+
+        let is_exp_char = |ch: char| -> bool {
+            return ch == 'e' || ch == 'E';
+        };
+
+        while ind < chars_arr.len() {
+            let ch: char = chars_arr[ind];
+
+            if ch == '+' || ch == '-' {
+                if val_str.is_empty() || is_exp_char(val_str.chars().last().unwrap()) {
+                    val_str.push(ch);
+                    ind += 1;
+                    continue;
+                }
+                // e
+                println!("{}", "Invalid character in number!");
+                return Err(NOT_VALID_JSON_FORMAT);
+            }
+
+            if ch.is_ascii_digit() {
+                val_str.push(ch);
+                ind += 1;
+                continue;
+            }
+
+            if ch == '.' {
+                if have_decimal_point {
+                    // e
+                    println!("{}", "Multiple decimal points in a number!");
+                    return Err(NOT_VALID_JSON_FORMAT);
+                }
+
+                val_str.push(ch);
+                have_decimal_point = true;
+                ind += 1;
+                continue;
+            }
+
+            if is_exp_char(ch) {
+                if have_exp_char {
+                    // e
+                    println!("{}", "Multiple decimal points in a number!");
+                    return Err(NOT_VALID_JSON_FORMAT);
+                }
+
+                val_str.push(ch);
+                have_exp_char = true;
+                ind += 1;
+                continue;
+            }
+
+            break;
+        }
+
+        return Ok(val_str);
+    }
+
     fn parse(
         chars_array: &Vec<char>,
         mut ind: usize,
         map: &mut HashMap<String, String>,
     ) -> Result<usize, &'static str> {
         let mut stack: Vec<String> = Vec::new();
-        /*let mut escape: bool = false;
-        let mut unicode_seq: String = String::new();
-        let mut cur_phase: ParsePhase = ParsePhase::ObjectSpace;
-        let mut unicode_rem_cnt: i8 = 0;
-
-        let in_string_phase = |_cur_phase: &ParsePhase| -> bool {
-            return matches!(_cur_phase, ParsePhase::KeyString | ParsePhase::ValueString);
-        };*/
 
         while ind < chars_array.len() {
             let ch: char = chars_array[ind];
 
-            if ch == '{' {
-                /*if in_string_phase(&cur_phase) {
-                    stack.last_mut().unwrap().push(ch);
-                } else {*/
-                if !stack.is_empty() {
-                    // e
-                    println!("{}", "Stack isn't empty!");
-                    return Err(NOT_VALID_JSON_FORMAT);
-                }
-
-                stack.push(String::from(ch));
-                // }
-                continue;
-            }
-
             if stack.is_empty() {
+                if ch == '{' {
+                    stack.push(String::from(ch));
+                    ind += 1;
+                    continue;
+                }
                 // e
                 println!("{}", "Stack is empty!");
                 return Err(NOT_VALID_JSON_FORMAT);
             }
 
-            // let last_element: String = stack.last().unwrap().clone();
-
             if ch == '"' {
                 let mut val_str: String = String::new();
                 let str_end: usize = Self::extract_string(&mut val_str, &chars_array, ind + 1)?;
                 stack.push(val_str);
-                ind = str_end + 1;
 
                 if stack.len() == 4 {
-                    map.insert(stack[1].to_string(), stack[3].to_string());
+                    map.insert(stack[1].clone(), stack[3].clone());
                     stack.drain(1..);
                 } else if stack.len() != 2 {
                     // e
@@ -222,30 +276,8 @@ impl JsonParser {
                     return Err(NOT_VALID_JSON_FORMAT);
                 }
 
+                ind = str_end + 1;
                 continue;
-
-                /*if in_string_phase(&cur_phase) {
-                    cur_phase = ParsePhase::ObjectSpace;
-                    stack.last_mut().unwrap().push(ch);
-
-                    if stack.len() == 4 {
-                        map.insert(stack[1].to_string(), stack[3].to_string());
-                        stack.drain(1..);
-                    } else if stack.len() != 2 {
-                        // e
-                        println!("{}", "Stack length is neither 2 nor 4!");
-                        return Err(NOT_VALID_JSON_FORMAT);
-                    }
-                } else if last_element == "{" {
-                    cur_phase = ParsePhase::KeyString;
-                } else if last_element == ":" {
-                    cur_phase = ParsePhase::ValueString;
-                } else {
-                    // e
-                    println!("{}: {}", "Invalid character encountered! (1)", last_element);
-                    return Err(NOT_VALID_JSON_FORMAT);
-                }
-                continue;*/
             }
 
             if ch == ':' {
@@ -256,30 +288,32 @@ impl JsonParser {
                 }
 
                 stack.push(String::from(ch));
+                ind += 1;
                 continue;
             }
 
-            if ch.is_ascii_digit() || ch == '-' {
+            if ch == 'n' || ch == 't' || ch == 'f' {
+                let val_str: String = Self::is_null_or_bool_sequence(&chars_array, ind)?;
+                ind = ind + val_str.len();
+                stack.push(val_str);
                 continue;
             }
 
-            if ch == 'n' {
-                continue;
-            }
-
-            if ch == 't' || ch == 'f' {
+            if ch.is_ascii_digit() || ch == '-' || ch == '+' {
+                let val_str: String = Self::is_num_sequence(&chars_array, ind)?;
+                ind = ind + val_str.len();
+                stack.push(val_str);
                 continue;
             }
 
             if ch == '[' {
+                /*let val_str: String = Self::is_array_sequence(&chars_array, ind)?;
+                ind = ind + val_str.len();
+                stack.push(val_str);*/
                 continue;
             }
 
             if ch == '{' {
-                continue;
-            }
-
-            if ch == ',' {
                 continue;
             }
 
@@ -296,40 +330,6 @@ impl JsonParser {
             }
 
             ind += 1;
-
-            /*if !matches!(cur_phase, ParsePhase::ObjectSpace) {
-                if in_string_phase(&cur_phase) {
-                    if ch == '\\' {
-                        escape = true;
-                    } else {
-                        stack.last_mut().unwrap().push(ch);
-                    }
-                } else {
-                    match cur_phase {
-                        ParsePhase::ValueArray => {}
-                        ParsePhase::ValueBoolean => {}
-                        ParsePhase::ValueNull => {}
-                        ParsePhase::ValueNumber => {}
-                        ParsePhase::ValueObject => {}
-                        _ => {}
-                    }
-                }
-            } else*/
-            /*if ch == ':' {
-                stack.push(String::from(ch));
-            } else if ch == '-' || ch.is_ascii_digit() {
-                if matches!(cur_phase, ParsePhase::ValueNumber) && ch == '-' {
-                    // e
-                    println!("{}", "Invalid character encountered! (2)");
-                    return Err(NOT_VALID_JSON_FORMAT);
-                }
-                if !matches!(cur_phase, ParsePhase::ValueNumber) {
-                    cur_phase = ParsePhase::ValueNumber;
-                    stack.push(String::from(ch));
-                } else {
-                    stack.last_mut().unwrap().push(ch);
-                }
-            }*/
         }
 
         if stack.len() != 1 {
